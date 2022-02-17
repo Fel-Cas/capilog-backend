f73f428b545a0ab858331167333f2b526a834a97
jhonquitian1305 <jhon.quitian@udea.edu.co>
2022-02-17 16:54:57 -0500

    test: Add first unit tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { UsersModule } from './pruebas/users/users.module';
 
 @Module({
   imports: [
---
+++ src/users/users-service-mock.ts
@@ -0,0 +1,17 @@
+// import { CreateUserDto } from "./dto"
+// import { User } from "./entities"
+
+// export class UsersServiceMock {
+//     async create(createUserDto: CreateUserDto): Promise<User> {
+//         return Promise.resolve({
+//           id: Math.random() * (1000 - 1) + 1,
+//           ...createUserDto
+//         })
+//     }
+//       update: jest.fn((id, dto) => {
+//         return {
+//           id,
+//           ...dto,
+//         }
+//       })
+// }
