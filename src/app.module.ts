2b003c82fef6e1af1ea63c58034ba0839b9d4228
jhonquitian1305 <jhon.quitian@udea.edu.co>
2022-02-17 22:53:37 -0500

    fix: fixed bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
-import { UsersModule } from './pruebas/users/users.module';
 
 @Module({
   imports: [
